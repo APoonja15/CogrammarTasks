6481b15960188f0e472da4ec69114a7efc5ee7c7
APoonja15 <85557003+APoonja15@users.noreply.github.com>
2024-03-17 13:47:09 +0000

    file to be corrected

---
+++ error_file.py
@@ -0,0 +1,12 @@
+import math
+
+#initialise variables with user input
+
+number = int(input("please enter base number: "))
+power = int(input("please enter power number: "))
+
+
+# use math function to calculate result
+result = math.pow(number, power)
+
+print(result)
